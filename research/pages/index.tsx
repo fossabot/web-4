b9bde734837b468ae350c7d4bb93f364bb4139d1
Andreas Thomas <dev@chronark.com>
2020-07-13 11:06:14 +0200

    basic setup

---
+++ research/pages/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Link from "next/link";
+import { Layout } from "@perfolio/design/components";
+import { NextPage } from "next";
+
+
+
+const BlogIndex: NextPage<Props> = () => {
+  return (
+    <Layout>
+     <h1>Hello World</h1>
+    </Layout>
+  );
+};
+
+export default BlogIndex;
