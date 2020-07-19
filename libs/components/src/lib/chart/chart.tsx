f4aeaa279964c740c19128e2c2390db7249acea5
Andreas Thomas <dev@chronark.com>
2020-07-19 18:52:24 +0200

    WIP experiment with chart as hero bg

--- libs/components/src/lib/chart/chart.tsx
+++ libs/components/src/lib/chart/chart.tsx
@@ -1,5 +1,4 @@
-import React, { createRef, useEffect } from "react"
-import dynamic from "next/dynamic"
+import React, { createRef, useEffect, useState } from "react"
 export interface ChartProps {
   data: { time: string; value: number }[]
   className?: string
@@ -7,15 +6,74 @@ export interface ChartProps {
 
 export const Chart = (props: ChartProps) => {
   const myref = createRef<HTMLDivElement>()
-  let lightweightCharts
+  const [series, setSeries] = useState(undefined)
 
   useEffect(() => {
-    lightweightCharts = require("lightweight-charts")
-    const { createChart } = lightweightCharts
-    const chart = createChart(myref.current)
-    const lineSeries = chart.addLineSeries()
-    lineSeries.setData(props.data)
+    if (!series) {
+      const { createChart } = require("lightweight-charts")
+      const chart = createChart(myref.current)
+      const s = chart.addAreaSeries({
+        localization: {
+          dateFormat: "dd/MM/yyyy",
+        },
+      })
+      chart.applyOptions({
+        layout: {
+          backgroundColor: "#F7FAFC",
+        },
+        priceScale: {
+          scaleMargins: {
+            top: 0.35,
+            bottom: 0.2,
+          },
+          borderVisible: false,
+        },
+        timeScale: {
+          borderVisible: false,
+        },
+        grid: {
+          horzLines: {
+            color: "#eee",
+            visible: false,
+          },
+          vertLines: {
+            color: "#ffffff",
+          },
+        },
+        crosshair: {
+          horzLine: {
+            visible: false,
+            labelVisible: false,
+          },
+          vertLine: {
+            visible: true,
+            style: 0,
+            width: 2,
+            color: "rgba(32, 38, 46, 0.1)",
+            labelVisible: false,
+          },
+        },
+      })
+      s.applyOptions({
+        lineColor: "rgba(0, 49, 153, 1)",
+        topColor: "rgba(0, 81, 255, 0.4)",
+        bottomColor: "rgba(153, 185, 255, 0.0)",
+        lineStyle: 1,
+        lineWidth: 2,
+        priceLineVisible: true,
+        priceLineWidth: 1,
+        priceLineColor: "#0051FF",
+        priceLineStyle: 3,
+      })
+      setSeries(s)
+    }
   }, [props.data])
+
+  useEffect(() => {
+    if (series) {
+      series.setData(props.data)
+    }
+  }, [props, series])
 
   return <div className={props.className} ref={myref} id="chart" />
 }
