0582cdecc99ab41645b0defc37378ea52c4806a5
Andreas Thomas <dev@chronark.com>
2020-07-23 19:37:43 +0200

    research index e2e test

--- apps/research-e2e/src/integration/index.test.ts
+++ apps/research-e2e/src/integration/index.test.ts
@@ -2,7 +2,6 @@ describe("research", () => {
   beforeEach(() => cy.visit("/"))
 
   it("should display header message", () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.contains("to enrich your research")
+    cy.contains("Enrich your research with Factor returns")
   })
 })
