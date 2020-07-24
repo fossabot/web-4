291e67fb9775b60c42007630378f185996abbf30
Andreas Thomas <dev@chronark.com>
2020-07-24 14:31:59 +0200

    Remove navbar from e2e test

--- apps/components-e2e/src/integration/navbar/navbar.spec.ts
+++
@@ -1,7 +0,0 @@
-describe("components: Navbar component", () => {
-  beforeEach(() => cy.visit("/iframe.html?id=navbar--primary"));
-
-  it("should render the component", () => {
-    cy.get("div").should("contain", "Perfolio");
-  });
-});
