31409018a64e82f9468a5eeb08be756d4dd2c079
Andreas Thomas <dev@chronark.com>
2020-07-24 22:46:26 +0200

    WIP Add editor buttons

--- libs/components/src/lib/code/code.tsx
+++ libs/components/src/lib/code/code.tsx
@@ -11,6 +11,11 @@ export interface CodeProps {
 export const Code = (props: CodeProps) => {
   return (
     <div className="px-4 m-8 overflow-visible bg-gray-900 rounded-lg shadow-lg ">
+      <div className="flex mt-4 -mb-4 space-x-2">
+        <span className="w-3 h-3 bg-red-500 rounded-full"></span>
+        <span className="w-3 h-3 bg-orange-300 rounded-full"></span>
+        <span className="w-3 h-3 bg-green-500 rounded-full"></span>
+      </div>
       <SyntaxHighlighter language={props.language} style={style} className="font-mono">
         {props.children}
       </SyntaxHighlighter>
