64066ae0e569a3cc3ecddc80d4a6b1f92d34bc01
Andreas Thomas <dev@chronark.com>
2020-07-27 09:02:39 +0200

    Remove default component tests
    They were taking upwards of 5 minutes for some reason and snapshot tests
    will cover them as well.

--- libs/components/src/lib/navbar/navbar.test.tsx
+++ libs/components/src/lib/navbar/navbar.test.tsx
@@ -1,13 +1,8 @@
 import React from "react"
-import { render } from "@testing-library/react"
 import renderer from "react-test-renderer"
 import Navbar from "./navbar"
 
 describe(" Navbar", () => {
-  it("should render successfully", () => {
-    const { baseElement } = render(<Navbar logo={<span>I am a logo</span>} />)
-    expect(baseElement).toBeTruthy()
-  })
   it("renders correctly", () => {
     const tree = renderer.create(<Navbar logo={<span>I am a logo</span>} />).toJSON()
     expect(tree).toMatchSnapshot()
