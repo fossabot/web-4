b1bf23a9784471c76b260d2dd7152d9995673176
Andreas Thomas <dev@chronark.com>
2020-07-27 11:36:51 +0200

    Use static mode

--- lighthouse/core.js
+++ lighthouse/core.js
@@ -1,18 +1,17 @@
 module.exports = {
-    ci: {
-      // assert: {
-      //   preset: "lighthouse:recommended",
-      // },
-      collect: {
-        numberOfRuns: 5,
-        url: [
-          "http://localhost:4200/"
-        ],
-        startServerCommand:"yarn nx run core:serve",
-      },
-      upload: {
-        target: "temporary-public-storage",
-      },
+  ci: {
+    // assert: {
+    //   preset: "lighthouse:recommended",
+    // },
+    collect: {
+      numberOfRuns: 5,
+      url: [
+        "http://localhost:4200/"
+      ],
+      staticDistDir:"dist/apps/core/exported",
     },
-  }
-  
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+}
